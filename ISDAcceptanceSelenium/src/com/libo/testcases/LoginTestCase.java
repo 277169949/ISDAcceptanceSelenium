c5ebee3eb417bf7e3bfc5e3e971e84bb78426c75
Administrator <Administrator@4PLMB6BMCZOZDWH>
2016-01-01 00:36:48 +0800

    test

---
+++ ISDAcceptanceSelenium/src/com/libo/testcases/LoginTestCase.java
@@ -0,0 +1,73 @@
+package com.libo.testcases;
+
+import java.util.concurrent.TimeUnit;
+
+import org.junit.*;
+
+import static org.junit.Assert.*;
+
+import org.openqa.selenium.*;
+
+import com.libo.tools.BrowserDriverTools;
+
+public class LoginTestCase {
+	private WebDriver driver;
+	private String baseUrl;
+	private boolean acceptNextAlert = true;
+	private StringBuffer verificationErrors = new StringBuffer();
+
+	@Before
+	public void setUp() throws Exception {
+		driver = BrowserDriverTools.getFirefoxDriver();
+		baseUrl = "http://isdp-acceptance-sit.huawei.com:8000/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+	@Test
+	public void testUntitled() throws Exception {
+		driver.get(baseUrl);
+
+	}
+
+	@After
+	public void tearDown() throws Exception {
+		// driver.quit();
+		String verificationErrorString = verificationErrors.toString();
+		if (!"".equals(verificationErrorString)) {
+			fail(verificationErrorString);
+		}
+	}
+
+	// private boolean isElementPresent(By by) {
+	// try {
+	// driver.findElement(by);
+	// return true;
+	// } catch (NoSuchElementException e) {
+	// return false;
+	// }
+	// }
+	//
+	// private boolean isAlertPresent() {
+	// try {
+	// driver.switchTo().alert();
+	// return true;
+	// } catch (NoAlertPresentException e) {
+	// return false;
+	// }
+	// }
+	//
+	// private String closeAlertAndGetItsText() {
+	// try {
+	// Alert alert = driver.switchTo().alert();
+	// String alertText = alert.getText();
+	// if (acceptNextAlert) {
+	// alert.accept();
+	// } else {
+	// alert.dismiss();
+	// }
+	// return alertText;
+	// } finally {
+	// acceptNextAlert = true;
+	// }
+	// }
+}
--- ISDAcceptanceSelenium/src/com/libo/tools/BrowserDriverTools.java
+++ ISDAcceptanceSelenium/src/com/libo/tools/BrowserDriverTools.java
@@ -1,12 +1,32 @@
 package com.libo.tools;
 
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+ 
 
 public class BrowserDriverTools {
 
 	public static WebDriver getChromeDriver() {
 
-		return null;
+		WebDriver driver=new  ChromeDriver();
+		 
+		return driver;
+	}
+	public static WebDriver getIEDriver() {
+
+		WebDriver driver=new  InternetExplorerDriver();
+		 
+		return driver;
 	}
 
+	
+	public static WebDriver getFirefoxDriver(){
+		WebDriver driver=new  FirefoxDriver();
+		driver.manage().window().maximize();		
+		return driver;
+	}
+	
+	
 }
